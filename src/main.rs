0856ff1550aad2f9f623d06658bd197c85cdcb48
Javier Porto <javierprtvel@gmail.com>
2025-11-12 20:14:47 +0100

    Dummy Search endpoint implementation

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,116 @@
-use anyhow::Context;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::extract::{Query, rejection::QueryRejection};
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Json, Router};
-
-use serde::Serialize;
+use chrono::{DateTime, Utc};
+use serde::ser::SerializeStruct;
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = Router::new()
-        .route("/", get(hello_json))
-        .layer(tower_http::catch_panic::CatchPanicLayer::new());
+    let router = Router::new()
+        .route("/", get(async || Json("Hello, world!")))
+        .route("/search", get(handle_search));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
-        .await
-        .context("failed to bind TCP listener")?;
-    axum::serve(listener, app)
-        .await
-        .context("axum::serve failed")?;
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
+    let listener = TcpListener::bind(addr).await?;
+
+    axum::serve(listener, router).await?;
 
     Ok(())
 }
 
-async fn hello_json() -> Result<(StatusCode, Json<Response>), AppError> {
-    let response = Response {
-        message: generate_message().context("failed to generate message")?,
-    };
+async fn handle_search(
+    params: Result<Query<SearchParams>, QueryRejection>,
+) -> Result<Json<ApiResponse<SearchResponse>>, (StatusCode, Json<ApiResponse<()>>)> {
+    match params {
+        Err(_err) => {
+            println!("Search query params are invalid");
+            let error_response = ErrorResponse {
+                code: "11".to_string(),
+                message: "Missing required params".to_string(),
+            };
+            Err((
+                StatusCode::BAD_REQUEST,
+                Json(ApiResponse::<()>::Ko(error_response)),
+            ))
+        }
+        Ok(query) => {
+            println!("Search query params are: {query:?}");
+            let dummy_response = SearchResponse {
+                events: vec![SearchEventResponse {
+                    id: "3fa85f64-5717-4562-b3fc-2c963f66afa6".to_string(),
+                    title: "Quevedo".to_string(),
+                    start_date: "2025-11-12".to_string(),
+                    start_time: "22:38:19".to_string(),
+                    end_date: "2025-11-12".to_string(),
+                    end_time: "14:45:15".to_string(),
+                    min_price: 15.99f64,
+                    max_price: 39.99f64,
+                }],
+            };
 
-    Ok((StatusCode::OK, Json(response)))
+            Ok(Json(ApiResponse::Ok(dummy_response)))
+        }
+    }
 }
 
-fn generate_message() -> anyhow::Result<&'static str> {
-    if rand::random() {
-        anyhow::bail!("no message for you");
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct SearchParams {
+    start_time: DateTime<Utc>,
+    end_time: DateTime<Utc>,
+}
+
+enum ApiResponse<T: Serialize> {
+    Ok(T),
+    Ko(ErrorResponse),
+}
+
+impl<T: serde::ser::Serialize> Serialize for ApiResponse<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        let mut state = serializer.serialize_struct("ApiResponse", 2)?;
+
+        match self {
+            ApiResponse::Ok(data) => {
+                state.serialize_field("data", data)?;
+                state.serialize_field("error", &Option::<ErrorResponse>::None)?;
+            }
+            ApiResponse::Ko(error) => {
+                state.serialize_field("data", &Option::<T>::None)?;
+                state.serialize_field("error", error)?;
+            }
+        }
+
+        state.end()
     }
-    Ok("Hello, world!")
 }
 
 #[derive(Serialize)]
-struct Response {
-    message: &'static str,
+struct ErrorResponse {
+    code: String,
+    message: String,
 }
 
-struct AppError(anyhow::Error);
-
-// This allows ? to automatically convert anyhow::Error to AppError
-impl From<anyhow::Error> for AppError {
-    fn from(value: anyhow::Error) -> Self {
-        Self(value)
-    }
+#[derive(Serialize)]
+struct SearchResponse {
+    events: Vec<SearchEventResponse>,
 }
 
-impl IntoResponse for AppError {
-    fn into_response(self) -> axum::response::Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
-    }
+#[derive(Serialize)]
+struct SearchEventResponse {
+    id: String,
+    title: String,
+    start_date: String,
+    start_time: String,
+    end_date: String,
+    end_time: String,
+    min_price: f64,
+    max_price: f64,
 }
---
+++ test/test_endpoints.py
@@ -0,0 +1,68 @@
+import requests
+
+def test_root():
+    resp = requests.get("http://localhost:8080/")
+    assert resp.status_code == 200
+    assert resp.json() == "Hello, world!"
+
+def test_search_returns_events_within_time_range():
+    params = {"start_time": "2025-11-12T08:00:00Z", "end_time": "2025-11-12T18:00:00Z"}
+
+    resp = requests.get("http://localhost:8080/search", params=params)
+    
+    assert resp.status_code == 200
+    expected_json = {
+        "data": {
+            "events": [
+                {
+                    "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    "title": "Quevedo",
+                    "start_date": "2025-11-12",
+                    "start_time": "22:38:19",
+                    "end_date": "2025-11-12",
+                    "end_time": "14:45:15",
+                    "min_price": 15.99,
+                    "max_price": 39.99,
+                }
+            ]
+        },
+        "error": None,
+    }
+
+    print(f"Response is {resp.json()}")
+    assert resp.json() == expected_json
+
+def test_search_returns_client_error_when_required_param_is_missing():
+    params = {"start_time": "2025-11-12T08:00:00Z"}
+
+    resp = requests.get("http://localhost:8080/search", params=params)
+
+    assert resp.status_code == 400
+    expected_json = {
+        "data": None,
+        "error": {"code": "11", "message": "Missing required params"},
+    }
+
+    print(f"Response is {resp.json()}")
+    assert resp.json() == expected_json
+
+def test_search_returns_server_error_when_something_unexpected_happens():
+    params = {"start_time": "2025-11-12T08:00:00Z", "end_time": "2025-11-12T18:00:00Z"}
+
+    resp = requests.get("http://localhost:8080/search", params=params)
+
+    assert resp.status_code == 500
+    expected_json = {
+        "data": None,
+        "error": {"code": "string", "message": "string"},
+    }
+
+    print(f"Response is {resp.json()}")
+    assert resp.json() == expected_json
+
+if __name__ == "__main__":
+    test_root()
+    test_search_returns_events_within_time_range()
+    test_search_returns_client_error_when_required_param_is_missing()
+    # test_search_returns_server_error_when_something_unexpected_happens()
+    print("All tests passed.")
