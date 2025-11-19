cec953b516780cbd4c4dcd39ae07f7ab05005f3e
Javier Porto <javierprtvel@gmail.com>
2025-11-19 19:03:19 +0100

    Shutdown gracefully, trace and timeout requests

--- src/infrastructure/adapters/controller/mod.rs
+++ src/infrastructure/adapters/controller/mod.rs
@@ -3,14 +3,18 @@ mod handlers;
 mod state;
 
 use std::sync::Arc;
+use std::time::Duration;
 
 use axum::Router;
 
 use state::ApplicationState;
+use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
 
 use crate::application::ports::provider::EventProviderClient;
 use crate::application::ports::repository::EventRepository;
 use crate::application::service::{IngestEventService, SearchEventService};
+
+const REQUEST_TIMEOUT_SECS: u64 = 30;
 
 pub async fn init_controller<T, S>(
     search_event_service: SearchEventService<T>,
@@ -21,7 +25,9 @@ where
     S: EventProviderClient + Send + Sync + 'static,
 {
     let state = init_application_state(search_event_service, ingest_event_service).await;
-    let app = api::configure(Arc::new(state));
+    let app = api::configure(Arc::new(state))
+        .layer(TimeoutLayer::new(Duration::from_secs(REQUEST_TIMEOUT_SECS)))
+        .layer(TraceLayer::new_for_http());
     Ok(app)
 }
 
--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -7,7 +7,9 @@ use std::{
 };
 
 use chrono::DateTime;
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, signal};
+use tracing::{Level, level_filters::LevelFilter};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 use uuid::Uuid;
 
 use crate::{
@@ -34,18 +36,28 @@ pub fn serve_app() -> anyhow::Result<()> {
             // Dependency Injection
             let shared_event_repository = Arc::new(event_repository);
             let shared_event_provider_client = Arc::new(event_provider_client);
-
             let search_event_service = SearchEventService::new(shared_event_repository.clone());
             let ingest_event_service =
                 IngestEventService::new(shared_event_provider_client, shared_event_repository);
 
+            // Controller
             let app =
                 adapters::controller::init_controller(search_event_service, ingest_event_service)
                     .await?;
 
+            // Tracing
+            let subscriber = tracing_subscriber::registry()
+                .with(LevelFilter::from_level(Level::TRACE))
+                .with(fmt::Layer::default());
+            subscriber.init();
+
+            // Server
             let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
             let listener = TcpListener::bind(addr).await?;
-            axum::serve(listener, app).await?;
+
+            axum::serve(listener, app)
+                .with_graceful_shutdown(shutdown_signal())
+                .await?;
 
             Ok::<(), anyhow::Error>(())
         })?;
@@ -53,6 +65,29 @@ pub fn serve_app() -> anyhow::Result<()> {
     Ok(())
 }
 
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl-C handler");
+    };
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install SIGTERM handler")
+            .recv()
+            .await;
+    };
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {println!("SIGINT received")},
+        _ = terminate => {println!("SIGTERM received")},
+    }
+}
+
+#[allow(dead_code)]
 async fn seed_event_repository<T: EventRepository>(event_repository: &T) {
     event_repository
         .upsert(Event {
